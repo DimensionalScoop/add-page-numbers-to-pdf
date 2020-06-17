cde1e8c4ab4fd80b237d61a46070a25f818659e6
Max Pernklau <DimensionalScoop@users.noreply.github.com>
2020-06-17 17:30:21 +0200

    clean up latex files after build

--- number_pdf.sh
+++ number_pdf.sh
@@ -4,7 +4,8 @@
 
 # write latex document
 mkdir -p build
-LATEX='build/auto_numbers'
+cd build
+LATEX='auto_numbers'
 echo '\documentclass[12pt,a4paper]{article}' > $LATEX
 echo '\usepackage{multido}' >> $LATEX
 echo '\usepackage[hmargin=.8cm,vmargin=1.5cm,nohead,nofoot]{geometry}' >> $LATEX
@@ -12,8 +13,9 @@ echo '\begin{document}' >> $LATEX
 echo "\multido{}{$1}{\vphantom{x}\newpage}" >> $LATEX
 echo '\end{document}' >> $LATEX
 
-pdflatex $LATEX # generate empty pages with page numbers on them
-pdftk $2 multistamp $LATEX.pdf output $3 # stamp the page number onto every page
+pdflatex $LATEX  # generate empty pages with page numbers on them
+cd ..
+pdftk $2 multistamp build/$LATEX.pdf output $3 # stamp the page number onto every page
 
 # clean up latex stuff
 rm -r build
